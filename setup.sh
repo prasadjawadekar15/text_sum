7c8980feed9e497287e50626bdbf6a92f6e55279
prasadjawadekar15 <73275873+prasadjawadekar15@users.noreply.github.com>
2025-07-13 03:27:36 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 python -m nltk.downloader punkt
 python -m nltk.downloader stopwords
+python -m nltk.downloader wordnet
+python -m nltk.downloader omw-1.4
+python -m nltk.downloader punkt_tab
